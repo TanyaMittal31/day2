43fdb271a473497f971a31c949dabd616c9ab5de
Tanya Agrawal <82451146+TanyaMittal31@users.noreply.github.com>
2022-04-08 22:46:54 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Burger</li>
+      <li>Naan - DalMakhani</li>
+      <li>Idli Dosa</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
